51d303cc80c9765280e3113dafdca4af4dc60ff2
Development <dev@jay.local>
2016-01-19 21:36:53 -0500

    initial gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const livereload = require('gulp-livereload')
+const nodemon    = require('gulp-nodemon')
+const eslint     = require('gulp-eslint')
+const gulp       = require('gulp')
+
+
+// TASKS ================================
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+})
+
+
+// nodemon ./bin/www
+gulp.task('start', ['lint'], () => {
+    nodemon({
+        script: 'bin/www',
+        ext: 'js ejs',
+        env: {'NODE_ENV': 'development'}
+    })
+})
+
+
+
+gulp.task('livereload', ['lint', 'start'], () => {
+    livereload.listen()
+    gulp.watch(['usr/views/*.ejs']).on('change', livereload.changed)
+})
+
+gulp.task('default', ['lint', 'start', 'livereload'], () => {})
