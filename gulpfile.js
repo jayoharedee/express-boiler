01170b4476a4d9b410faf88a15a4e49bcd924b0e
jayoharedee <shme@protonmail.com>
2016-02-04 17:51:40 -0500

    removed nodemon in preparation for pm2 production set up

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,43 @@
 const livereload = require('gulp-livereload')
 const nodemon    = require('gulp-nodemon')
 const eslint     = require('gulp-eslint')
+const rename     = require('gulp-rename')
+const sass       = require('gulp-sass')
 const gulp       = require('gulp')
-
 
 // TASKS ================================
 gulp.task('lint', () => {
     return gulp.src(['**/*.js', '!node_modules/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
 })
 
-
-// nodemon ./bin/www
-gulp.task('start', ['lint'], () => {
-    nodemon({
-        script: 'bin/www',
-        ext: 'js ejs',
-        env: {'NODE_ENV': 'development'}
-    })
+//sass to css magic
+gulp.task('sass', () => {
+    return gulp.src('srv/sass/style.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('srv/css'))
 })
 
-
-// livereload
-gulp.task('livereload', ['lint', 'start'], () => {
+//livereload
+gulp.task('livereload', ['lint'], () => {
     livereload.listen()
-    gulp.watch(['usr/views/*.ejs']).on('change', livereload.changed)
 })
 
-gulp.task('default', ['lint', 'start', 'livereload'])
+// these watchers will not be needed when using pm2's watching feature
+// create new git branch for master repo and continue on in dev with this one
+//watchers
+gulp.task('watch', () => {
+    gulp.watch(['usr/views/**', 'srv/**']).on('change', livereload.changed)
+    gulp.watch('srv/sass/**', ['sass'])
+})
+
+// default
+gulp.task('default', [
+    'watch',
+    'lint',
+    'sass'
+    'livereload'
+])
