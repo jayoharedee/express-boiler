1c88dd358eac5af319465884565ed5de7f70d771
jayoharedee <shme@protonmail.com>
2016-02-06 18:06:37 -0500

    fixed test merge

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,10 @@
 'use strict'
 
-<<<<<<< HEAD
-=======
 const eslint     = require('gulp-eslint')
->>>>>>> development
 const rename     = require('gulp-rename')
 const sass       = require('gulp-sass')
 const gulp       = require('gulp')
 
-<<<<<<< HEAD
-// sass to css magic
-gulp.task('sass', () => {
-    return gulp.src('srv/sass/style.sass')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('srv/css'))
-=======
 // TASKS ================================
 gulp.task('lint', () => {
     return gulp.src(['**/*.js', '!node_modules/**'])
@@ -30,14 +19,10 @@ gulp.task('sass', () => {
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('srv/css'))
->>>>>>> development
 })
 
 // default
 gulp.task('default', [
-<<<<<<< HEAD
-=======
     'lint',
->>>>>>> development
     'sass'
 ])
