5817b44efb7dbf92e7a7aa2a39edb741558dd801
jayoharedee <shme@protonmail.com>
2016-02-06 17:50:34 -0500

    removed livereload and watchers cause pm2 handles that now

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const livereload = require('gulp-livereload')
-const nodemon    = require('gulp-nodemon')
 const eslint     = require('gulp-eslint')
 const rename     = require('gulp-rename')
 const sass       = require('gulp-sass')
@@ -15,7 +13,7 @@ gulp.task('lint', () => {
     .pipe(eslint.failAfterError())
 })
 
-//sass to css magic
+// sass to css magic
 gulp.task('sass', () => {
     return gulp.src('srv/sass/style.sass')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
@@ -23,23 +21,8 @@ gulp.task('sass', () => {
     .pipe(gulp.dest('srv/css'))
 })
 
-//livereload
-gulp.task('livereload', ['lint'], () => {
-    livereload.listen()
-})
-
-// these watchers will not be needed when using pm2's watching feature
-// create new git branch for master repo and continue on in dev with this one
-//watchers
-gulp.task('watch', () => {
-    gulp.watch(['usr/views/**', 'srv/**']).on('change', livereload.changed)
-    gulp.watch('srv/sass/**', ['sass'])
-})
-
 // default
 gulp.task('default', [
-    'watch',
     'lint',
     'sass'
-    'livereload'
 ])
