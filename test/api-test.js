ead5bc47b2a2411905395e487eda90239a4ac337
jayoharedee <shme@protonmail.com>
2016-02-11 18:20:07 -0500

    included tests for root API calls and also 404s

--- test/api-test.js
+++ test/api-test.js
@@ -1,23 +1,35 @@
 'use strict'
 
-//TODO: expand on all these tests and build out the /test folder for
-// further app functionality
 // Remember, this is only a very basic boilerplate to get apps off the ground
 
-const should = require('chai').should()
+// const should = require('chai').should()
 const req    = require('supertest')
 
 const app    = require('../server')
 
-/* these routes will be built out and refined a little bit more when the
-   project is extended */
+/**
+ * These are only basic tests to ensure boiler API
+ * functionality is working prior to site start
+ */
 describe('Routes', function() {
     it('must respond with 200 for /', function(done) {
         req(app)
             .get('/')
-            .set('Accept', 'application/json')
+            .set('Accept', 'text/html')
             .expect(200)
             .expect('Content-Type', 'text/html; charset=utf-8')
+            .end(function(err, res) {
+                if (err) { return done(err) }
+                done()
+            })
+    })
+
+    it('must respond with 200 for API call to /', function(done) {
+        req(app)
+            .get('/')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .expect('Content-Type', 'application/json; charset=utf-8')
             .end(function(err, res) {
                 if (err) { return done(err) }
                 done()
@@ -29,6 +41,18 @@ describe('Routes', function() {
             .get('/home')
             .set('Accept', 'application/json')
             .expect(200)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .end(function(err, res) {
+                if (err) { return done(err) }
+                done()
+            })
+    })
+
+    it('must respond 404 to a route that does not exist', function(done) {
+        req(app)
+            .get('/wrongroute')
+            .set('Accept', 'application/json')
+            .expect(404)
             .expect('Content-Type', 'application/json; charset=utf-8')
             .end(function(err, res) {
                 if (err) { return done(err) }
