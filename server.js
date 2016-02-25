6f2b358387d8e430e76987fd102003ee92aae783
jayoharedee <shme@protonmail.com>
2016-02-25 18:59:41 -0500

    linted

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const express          = require('express')
 const app              = express()
-
 const expressValidator = require('express-validator')
 const bodyParser       = require('body-parser')
 const help             = require('./lib/helpers')
@@ -15,24 +14,28 @@ app.use(bodyParser.urlencoded({
 
 app.use(expressValidator())
 
-
 // configs
 require('./lib/config')(app)
 
-
 // ROUTES ===============================
 app.get('/', (req, res, next) => {
-    return res.render('index', {title: 'Express Boiler'})
+    if (req.accepts('html')) {
+        return res.render('index', {title: 'Express Boiler'})
+    }
+
+    return res.status(200).json({
+        status: 'Welcome to the API',
+        data: null,
+        message: 'Success'
+    })
 })
 
 require('./lib/routes')(app)
-
 
 // 404's
 app.use((req, res, next) => next(
     help.createError('Nothing to be found here.', 404)
 ))
-
 
 app.use((err, req, res, next) => {
     res.status(err.code || 500)
