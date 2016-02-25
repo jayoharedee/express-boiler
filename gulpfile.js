9189958eb76bbb4e5cacd6b7007500d6445cb695
jayoharedee <shme@protonmail.com>
2016-02-25 18:57:40 -0500

    added packages for dev builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const eslint     = require('gulp-eslint')
+const livereload = require('gulp-livereload')
+const nodemon    = require('gulp-nodemon')
 const rename     = require('gulp-rename')
-const sass       = require('gulp-sass')
+const eslint     = require('gulp-eslint')
 const gulp       = require('gulp')
+const sass       = require('gulp-sass')
 
 // TASKS ================================
 gulp.task('lint', () => {
@@ -11,6 +13,15 @@ gulp.task('lint', () => {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
+})
+
+// nodemon ./bin/www
+gulp.task('start', ['lint'], () => {
+    nodemon({
+        script: 'bin/www',
+        ext: 'js',
+        env: {'NODE_ENV': 'development'}
+    })
 })
 
 // sass to css magic
@@ -21,8 +32,22 @@ gulp.task('sass', () => {
     .pipe(gulp.dest('srv/css'))
 })
 
-// default
+// livereload
+gulp.task('livereload', ['lint', 'start'], () => {
+    livereload.listen()
+})
+
+// watchers
+gulp.task('watch', () => {
+    gulp.watch(['usr/views/**', 'srv/**']).on('change', livereload.changed)
+    gulp.watch('srv/sass/**', ['sass'])
+})
+
+// default task
 gulp.task('default', [
+    'start',
     'lint',
-    'sass'
+    'watch',
+    'sass',
+    'livereload'
 ])
